f46eedf402078db9e4595f88fbd86750f11ea328
Gopi12378 <22501a4242@pvpsit.ac.in>
2025-08-17 19:22:13 +0530

    Update Eventlist.jsx

--- auth-app/src/pages/Eventlist.jsx
+++ auth-app/src/pages/Eventlist.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./EventList.css";
+import "./Eventlist.css";
 
 export default function EventList() {
   const [events, setEvents] = useState([]);
